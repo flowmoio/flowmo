2e1f820e641341446a1599f77785f8dc93f850a2
Max Shen <m4xshen@gmail.com>
2025-04-18 11:23:53 +0800

    refactor(web): add swr config

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -3,12 +3,21 @@
 import { HeroUIProvider } from '@heroui/react';
 import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
+import { SWRConfig } from 'swr';
 
 export function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
+
   return (
-    <HeroUIProvider navigate={router.push} className="w-full">
-      {children}
-    </HeroUIProvider>
+    <SWRConfig
+      value={{
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }}
+    >
+      <HeroUIProvider navigate={router.push} className="w-full">
+        {children}
+      </HeroUIProvider>
+    </SWRConfig>
   );
 }
--- apps/web/src/hooks/useLogs.ts
+++ apps/web/src/hooks/useLogs.ts
@@ -2,10 +2,7 @@ import { useQuery } from '@supabase-cache-helpers/postgrest-swr';
 import supabase from '@/utils/supabase/client';
 
 export default function useLogs() {
-  const { data, isLoading } = useQuery(supabase.from('logs').select('*'), {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const { data, isLoading } = useQuery(supabase.from('logs').select('*'));
 
   return {
     logs: data ?? [],
