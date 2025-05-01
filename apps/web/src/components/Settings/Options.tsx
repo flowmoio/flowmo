22b8efc1097c73856d8f3f08ac5c77231f499f99
Max Shen <m4xshen@gmail.com>
2025-05-01 18:50:07 +0800

    feat(web): use NumberInput for break ratio

--- apps/web/src/components/Settings/Options.tsx
+++ apps/web/src/components/Settings/Options.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@heroui/button';
-import { Input } from '@heroui/input';
+import { NumberInput } from '@heroui/number-input';
 import { useState, useTransition } from 'react';
 import { toast } from 'sonner';
 import { updateOptions } from '@/actions/settings';
@@ -19,22 +19,16 @@ export default function Options({
       <div className="flex items-center gap-3">
         <h2 className="text-xl font-semibold">Options</h2>
       </div>
-      <Input
-        label="Break ratio"
-        labelPlacement="outside"
-        placeholder="5"
-        variant="bordered"
+      <NumberInput
+        label="Break Ratio"
         description="Your break time will be your focus time divided by this number."
         radius="sm"
-        type="number"
-        value={breakRatio.toString()}
-        onValueChange={(value) =>
-          setBreakRatio(value === '' ? 0 : parseInt(value, 10))
-        }
+        minValue={1}
+        value={breakRatio}
+        onValueChange={setBreakRatio}
         classNames={{
-          input: 'text-[16px]',
           inputWrapper:
-            'border-secondary data-[hover=true]:border-secondary data-[focus=true]:!border-primary',
+            'bg-secondary max-w-xs data-[hover=true]:bg-secondary data-[focus=true]:!bg-secondary',
         }}
       />
       <div className="flex">
@@ -42,8 +36,9 @@ export default function Options({
           color="primary"
           radius="sm"
           className="ml-auto mt-2"
-          isDisabled={breakRatio <= 0}
+          isDisabled={breakRatio === defaultBreakRatio}
           isLoading={isLoading}
+          disableRipple
           onPress={() => {
             startTransition(async () => {
               const { error } = await updateOptions(breakRatio);
